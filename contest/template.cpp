02eee57e4d8d9aceb7644cc2f6ea606c7f83c502
SnexusG <swapnilgore029@gmail.com>
2021-04-06 18:49:12 +0530

    new changes

--- contest/template.cpp
+++ contest/template.cpp
@@ -8,13 +8,10 @@ template <typename T> void print(T t) { cout<<t<<endl; }
 #define ll long long
 #define vll vector<ll>
 #define get(a, n)    for(ll it = 0; it < n; it++)    cin>>a[it];
-//creating a 2d vector containing n vectors of size m each : vector<vll> vec(n, vll(m,0));
 //ascii of a is 97
-//creating a vector with elements : vector<long long> arr{1,2,3,4};
 //converting string to num : stringstream geek(s); int x = 0; geek >> x; 
 
 ll n = 0, t = 0; ll small = -1e18, big = 1e18; const ll mod=1e9+7; map<ll,ll> occ;
-//global variables end  
 
 void print2dVec(vector<vector<ll>> v){for(int i = 0; i < v.size(); ++i){cout<<i<<" : ";for(int j = 0; j < v[i].size(); ++j){cout<<v[i][j]<<" ";}print("");}}
 
@@ -33,10 +30,9 @@ bool isprime(ll p){for(int i = 2; i*i <= p; ++i){if(p%i==0 && p != i){return fal
 }
 
 bool sortVec( const vector<ll>& v1, const vector<ll>& v2 ) { 
-//change 0 to 1 to sort as per second column
-//change < sign to > sign to sort in descending 
 return v1[0] < v2[0]; 
 } 
+
 //useful functions end
 
 int main(){
